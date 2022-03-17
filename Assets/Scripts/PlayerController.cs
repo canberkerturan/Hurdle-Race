7b4c84f4ce5155434eb58906c2edc584414d1048
darwageddon <canberk.ertrn@hotmail.com>
2022-03-17 23:34:46 +0300

    Spacebar Jump Update
    Spacebar code was added in ordet to make the player jump when we tell it to by pressing spacebar.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,13 +8,16 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        playerRb = GetComponent<Rigidbody>();
-        playerRb.AddForce(Vector3.up * 1000);
+        playerRb = GetComponent<Rigidbody>();       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+        }
+
     }
 }
