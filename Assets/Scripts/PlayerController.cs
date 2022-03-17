e804fb2a38ddc4b7566f2cc5ceece2c1529b9d6f
darwageddon <canberk.ertrn@hotmail.com>
2022-03-17 23:25:42 +0300

    Player's Jump
    AddForce method was added to make player jump at start.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRb; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.AddForce(Vector3.up * 1000);
     }
 
     // Update is called once per frame
